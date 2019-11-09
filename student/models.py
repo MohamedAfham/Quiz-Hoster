af775a037bac7dfb9a167dc005d1c412522798c1
ThakeeNathees <thakeenathees@gmail.com>
2019-11-10 02:06:11 +0530

    user register front end crated

--- student/models.py
+++ student/models.py
@@ -1,7 +1,79 @@
 from django.db import models
 
+from django.contrib.auth.forms import UserCreationForm
+
+## depricate
+## student.index : pk, user.username : pk
+## student.index = user.username
+## student required first name, last name
+
+## TODO: delete student model, make user.username = index, user.first_name = name, first_name = required
+
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User 
 
 class Student(models.Model):
     student = models.OneToOneField(User, on_delete=models.CASCADE)
-    index = models.CharField(max_length=20)
+    index = models.CharField(
+        max_length=20, primary_key=True,
+        error_messages={
+            'unique': _("A user with that index already exists."),
+        },
+    )
+
+
+# Forms and Validators #################################
+from django import forms
+
+## validators
+from django.contrib.auth import (
+    authenticate, get_user_model, password_validation,
+)
+
+from django.core import validators
+from django.utils.deconstruct import deconstructible
+@deconstructible
+class UnicodeUsernameValidator(validators.RegexValidator):
+    regex = r'^[\w.@+-]+$'
+    message = _(
+        'Enter a valid username. This value may contain only letters, '
+        'numbers, and @/./+/-/_ characters.'
+    )
+    flags = 0
+
+## forms
+class StudentForm(forms.Form):
+    name = forms.CharField(
+        max_length=150, 
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[UnicodeUsernameValidator()],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    index = forms.CharField(max_length=20)
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 != password2:
+            raise forms.ValidationError(
+                "your passwords didn't match"
+            )
+        return cleaned_data
+
+
--- student/views.py
+++ student/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import StudentForm
+from django.contrib.auth.models import User
+from .models import Student
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            index = form.cleaned_data.get('index')
+            password = form.cleaned_data.get('password1')
+            ##user = User.objects.create_user(index, password=password, first_name=name)
+            messages.success(request, f'Account created for {name}, index : {index}, password : {password}')
+            return redirect('quiz-home')
+    else:
+        form = StudentForm()
+    return render(request, 'student_register.html', {'form':form})
