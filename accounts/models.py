cfbeb00e8ee5ded463e2cb0f3991f8f5b48ef014
ThakeeNathees <thakeenathees@gmail.com>
2019-11-20 13:41:24 +0530

    repo cleaned

--- accounts/models.py
+++ accounts/models.py
@@ -52,16 +52,6 @@ from django.contrib.auth import (
     authenticate, get_user_model, password_validation,
 )
 from django.utils.translation import gettext_lazy as _
-from django.core import validators
-from django.utils.deconstruct import deconstructible
-@deconstructible
-class UnicodeUsernameValidator(validators.RegexValidator):
-    regex = r'^[\w.@+-]+$'
-    message = _(
-        'Enter a valid username. This value may contain only letters, '
-        'numbers, and @/./+/-/_ characters.'
-    )
-    flags = 0
 
 
 ## forms
@@ -69,18 +59,10 @@ class StudentRegisterForm(forms.Form):
     name = forms.CharField(
         max_length=150, 
         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        validators=[UnicodeUsernameValidator()],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        },
     )
     index = forms.CharField(
         max_length=30, 
         help_text=_('Required. 30 characters or fewer. Letters will stored as Upper case'),
-        validators=[UnicodeUsernameValidator()],
-        error_messages={
-            'unique': _("A user with that index already exists."),
-        },
     )
     password1 = forms.CharField(
         label=_("Password"),
@@ -95,8 +77,8 @@ class StudentRegisterForm(forms.Form):
         help_text=_("Enter the same password as before, for verification."),
     )
 
-    def clean(self):
-        cleaned_data = super().clean()
+    def clean(self, *args, **kwargs):
+        cleaned_data = super().clean(*args, **kwargs)
         password1 = cleaned_data.get('password1')
         password2 = cleaned_data.get('password2')
 
@@ -111,10 +93,6 @@ class StudentLoginForm(forms.Form):
     index = forms.CharField(
         max_length=30, 
         help_text=_('Required. 30 characters or fewer.'),
-        validators=[UnicodeUsernameValidator()],
-        error_messages={
-            'unique': _("A user with that index already exists."),
-        },
     )
     password = forms.CharField(
         label=_("Password"),
